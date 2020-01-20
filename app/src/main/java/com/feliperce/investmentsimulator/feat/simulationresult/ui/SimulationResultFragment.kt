6ad149a044df344dff6bb85f90ca4abc41e39569
Felipe <felipe.rce@gmail.com>
2020-01-20 01:07:58 -0300

    nav action fix

--- app/src/main/java/com/feliperce/investmentsimulator/feat/simulationresult/ui/SimulationResultFragment.kt
+++ app/src/main/java/com/feliperce/investmentsimulator/feat/simulationresult/ui/SimulationResultFragment.kt
@@ -7,16 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 
 import com.feliperce.investmentsimulator.R
 import com.feliperce.investmentsimulator.databinding.FragmentSimulationResultBinding
 import com.feliperce.investmentsimulator.feat.simulationresult.viewModel.SimulationResultViewModel
+import kotlinx.android.synthetic.main.fragment_simulation_result.*
 import org.koin.android.ext.android.inject
 
-/**
- * A simple [Fragment] subclass.
- */
 class SimulationResultFragment : Fragment() {
 
     private val args: SimulationResultFragmentArgs by navArgs()
@@ -37,5 +36,11 @@ class SimulationResultFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.formatData(args.InvestmentArg)
+
+        simulateButton.setOnClickListener {
+            val action = SimulationResultFragmentDirections
+                .actionSimulationResultFragmentToSimulatorFragment()
+            findNavController().navigate(action)
+        }
     }
 }
